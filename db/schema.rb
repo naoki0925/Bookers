4f032d59b8ab1484027bed5f8f277f6daad5e930
EC2 Default User <ec2-user@ip-172-31-72-138.ec2.internal>
2021-01-28 07:13:48 +0000

    モデル作成　直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_155019) do
+ActiveRecord::Schema.define(version: 2021_01_28_070508) do
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
