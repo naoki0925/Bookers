b23e58b97db7c050887c2c383eac34c52f55e742
EC2 Default User <ec2-user@ip-172-31-72-138.ec2.internal>
2021-01-29 16:42:22 +0000

    [finish]編集画面、削除機能、index&new

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,7 +1,4 @@
 class TodolistsController < ApplicationController
-  def new
-    @book = Book.new
-  end
   
   def create
     book = Book.new(book_params)
@@ -11,11 +8,27 @@ class TodolistsController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def show
     @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to todolist_path(book.id)
   end  
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+  end
   
   private
   def book_params
