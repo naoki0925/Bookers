3c8320b49c7c906d0619042f103aaf7aaf2c9615
EC2 Default User <ec2-user@ip-172-31-72-138.ec2.internal>
2021-02-02 15:12:07 +0000

    Rspecのインストール

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,6 @@ ActiveRecord::Schema.define(version: 2021_01_28_160308) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
